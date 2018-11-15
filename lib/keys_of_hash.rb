b9cc67795aa82aafdd7a9cdceef68e4f3dafc343
Claudia Fox <claudiafox95@icloud.com>
2018-11-15 11:58:12 +0000

    automatically backed up by learn

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -2,7 +2,7 @@ class Hash
   def keys_of(*arguments)
     keys = []
     arguments.each do |argument|
-      self.each do |key, value|
+      self.each do |keys, value|
         if value == argument
           keys << key
         end
